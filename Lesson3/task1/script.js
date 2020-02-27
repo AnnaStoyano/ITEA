b3370827c50c3a708cb53f0696033247409372c7
Anna Stoyanova <annstoyanova99@gmail.com>
2020-02-27 17:39:52 +0200

    Task1

---
+++ Lesson3/task1/script.js
@@ -0,0 +1,42 @@
+let counterDiv = document.querySelector('.counter');
+let stopCounterBtn = document.querySelector('[name="stopCounter"]');
+let refreshCounterBtn = document.querySelector('[name="refreshCounter"]');
+let startCounterBtn = document.querySelector('[name="startCounter"]');
+let countDownBtn = document.querySelector('[name="countDown"]');
+let counter = 0;
+let startCount;
+
+startCounterBtn.onclick = function(){
+    startCount = setInterval(function(){
+        if(counter >= 30)
+           clearInterval(startCount);
+        else{
+            counter++;
+            writeToCounterDiv();
+        }
+    },300);
+}
+
+function writeToCounterDiv(){
+    counterDiv.innerHTML = counter;
+}
+
+stopCounterBtn.onclick = function(){
+    clearInterval(startCount);
+}
+
+refreshCounterBtn.onclick = function(){
+    counter = 0;
+    writeToCounterDiv();
+}
+
+countDownBtn.onclick = function(){
+    startCount = setInterval(function(){
+        if(counter <= 0)
+           clearInterval(startCount);
+        else{   
+            counter--;
+            writeToCounterDiv();
+        }
+    },300);
+}
