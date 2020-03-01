3470c23e0b4141ad2bb4332a0ef26f642ea17363
Anna Stoyanova <annstoyanova99@gmail.com>
2020-03-01 15:50:23 +0200

    Debuging

--- Lesson3/task1/script.js
+++ Lesson3/task1/script.js
@@ -1,4 +1,4 @@
-let counterDiv = document.querySelector('.counter');
+let counterDiv = document.querySelector(".counter");
 let stopCounterBtn = document.querySelector('[name="stopCounter"]');
 let refreshCounterBtn = document.querySelector('[name="refreshCounter"]');
 let startCounterBtn = document.querySelector('[name="startCounter"]');
@@ -6,37 +6,37 @@ let countDownBtn = document.querySelector('[name="countDown"]');
 let counter = 0;
 let startCount;
 
-startCounterBtn.onclick = function(){
-    startCount = setInterval(function(){
-        if(counter >= 30)
-           clearInterval(startCount);
-        else{
-            counter++;
-            writeToCounterDiv();
-        }
-    },300);
+startCounterBtn.onclick = function() {
+  clearInterval(startCount);
+  startCount = setInterval(function() {
+    if (counter >= 30) clearInterval(startCount);
+    else {
+      counter++;
+      writeToCounterDiv();
+    }
+  }, 300);
+};
+
+function writeToCounterDiv() {
+  counterDiv.innerHTML = counter;
 }
 
-function writeToCounterDiv(){
-    counterDiv.innerHTML = counter;
-}
+stopCounterBtn.onclick = function() {
+  clearInterval(startCount);
+};
 
-stopCounterBtn.onclick = function(){
-    clearInterval(startCount);
-}
+refreshCounterBtn.onclick = function() {
+  counter = 0;
+  writeToCounterDiv();
+};
 
-refreshCounterBtn.onclick = function(){
-    counter = 0;
-    writeToCounterDiv();
-}
-
-countDownBtn.onclick = function(){
-    startCount = setInterval(function(){
-        if(counter <= 0)
-           clearInterval(startCount);
-        else{   
-            counter--;
-            writeToCounterDiv();
-        }
-    },300);
-}
+countDownBtn.onclick = function() {
+  clearInterval(startCount);
+  startCount = setInterval(function() {
+    if (counter <= 0) clearInterval(startCount);
+    else {
+      counter--;
+      writeToCounterDiv();
+    }
+  }, 300);
+};
