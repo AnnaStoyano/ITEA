5f0bf1c65767e3c9e6179f29e71a970d1cd908ca
Anna Stoyanova <annstoyanova99@gmail.com>
2020-03-03 10:23:37 +0200

    window.onbeforeunload

--- lesson4/task2/script.js
+++ lesson4/task2/script.js
@@ -1,15 +1,24 @@
 var divArr = document.getElementsByTagName("div");
-for (var i = 0; i < divArr.length; i++) {
-  divArr[i].onclick = function() {
-    this.style.backgroundColor = "red";
-  };
-  divArr[i].onmouseover = function() {
-    // debugger;
-    let r = Math.round(Math.random() * 256);
-    let g = Math.round(Math.random() * 256);
-    let b = Math.round(Math.random() * 256);
-    this.style.backgroundColor = `rgb(${r},${g},${b})`;
-    this.innerHTML = `rgb(${r},${g},${b})`;
-    console.log(r, g, b);
-  };
+function eventsOnThePage() {
+  for (var i = 0; i < divArr.length; i++) {
+    divArr[i].onclick = function() {
+      this.style.backgroundColor = "red";
+    };
+    divArr[i].onmouseover = function() {
+      let r = Math.round(Math.random() * 256);
+      let g = Math.round(Math.random() * 256);
+      let b = Math.round(Math.random() * 256);
+      this.style.backgroundColor = `rgb(${r},${g},${b})`;
+      this.innerHTML = `rgb(${r},${g},${b})`;
+      console.log(r, g, b);
+    };
+  }
 }
+
+window.onload = function() {
+  this.eventsOnThePage();
+};
+
+window.onbeforeunload = function() {
+  return "Данные не сохранены.";
+};
